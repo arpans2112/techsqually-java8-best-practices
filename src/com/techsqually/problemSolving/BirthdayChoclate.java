2a96bd3a61d05325b84079a0cda85a0b88d79e5c
arpans2112 <arpan673523@gmail.com>
2019-08-29 21:13:24 -0500

    NestedTernaryOperatorExample is added Integer.class test is added BirthdayChocalate algorithm Solved using Stream operation - Good Example Must check that

--- src/com/techsqually/problemSolving/BirthdayChoclate.java
+++ src/com/techsqually/problemSolving/BirthdayChoclate.java
@@ -9,9 +9,29 @@ public class BirthdayChoclate {
     public static void main(String[] args) {
 
         List<Integer> s = new ArrayList<>(Arrays.asList(1,2 ,1 ,3, 2));
-        System.out.println(birthday(s,3,2));
+//        System.out.println(birthday(s,3,2));
+
+        System.out.println(birthDayChoclateDis(s,3,2));
 
     }
+
+
+    public static int birthDayChoclateDis(List<Integer> s, int d, int m){
+
+        int disStributionCount = 0;
+
+        for (int startIndex  = 0 ; s.size() - startIndex >= m ; startIndex++){
+
+            if (s.subList(startIndex,startIndex+m).stream().mapToInt(Integer::intValue).sum() == d){
+                disStributionCount++;
+            }
+
+        }
+
+        return disStributionCount;
+
+    }
+
 
 
     static int birthday(List<Integer> s, int d, int m) {
