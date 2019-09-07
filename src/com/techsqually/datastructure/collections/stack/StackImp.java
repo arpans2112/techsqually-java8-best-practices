469dfd9c6d53b8846bb9d28c55fdacb95789697f
arpans2112 <arpan673523@gmail.com>
2019-09-06 20:50:48 -0500

    StackImp.java is added

---
+++ src/com/techsqually/datastructure/collections/stack/StackImp.java
@@ -0,0 +1,51 @@
+package com.techsqually.datastructure.collections.stack;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class StackImp {
+
+ public static int[] stack = new int[10];
+  public static int tos ;
+
+      StackImp(){
+          tos = -1;
+      }
+
+    public static void main(String[] args) {
+
+        //Pusting Element into a Stack
+        IntStream.range(1,21).forEach(i -> push(i));
+
+        //Pop added Elements from Stack
+        for (int i = 0 ; i < stack.length-10; i++){
+
+            System.out.println(pop());
+        }
+
+
+    }
+
+
+    public static void push(int item){
+        if (tos ==  stack.length-1){
+            int[] backUpStack = new int[stack.length + 10];
+            System.arraycopy(stack,0,backUpStack,0,stack.length);
+            stack = backUpStack;
+            stack[++tos] = item;
+        }else{
+            stack[++tos] = item;
+        }
+    }
+
+    public static int pop(){
+
+          if (tos < 0){
+              System.out.println("Stack underflow.");
+              return 0;
+          }else{
+              return stack[tos--];
+          }
+    }
+
+}
